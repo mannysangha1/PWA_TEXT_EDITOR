e167d1d8f164409d33e1eb65303d504bfd1bb889
Manny <mannysangha020@gmail.com>
2022-11-22 21:30:10 -0800

    added if statement

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -13,6 +13,11 @@ window.addEventListener('beforeinstallprompt', (event) => {
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+    
 });
 
 // TODO: Add an handler for the `appinstalled` event
