f1fa82af5f65d527e9ba8bc3dd4383c3e6860d2b
Manny <mannysangha020@gmail.com>
2022-11-22 21:06:01 -0800

    added const tx

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,6 +25,7 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const jatedb = await openDB('jate', 1);
+  const tx = jatedb.transaction('jate', 'readonly');
 }
 
 initdb();
