4f9470ce37401b61987b636940250b854fbb7bf3
Manny <mannysangha020@gmail.com>
2022-11-22 21:26:38 -0800

    added const promptEvent to buttInstall.addEventListener

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -11,7 +11,9 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
