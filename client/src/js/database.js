445e0a78d967c4c2fc76efd6b82627bde091d2a7
Manny <mannysangha020@gmail.com>
2022-11-22 20:59:08 -0800

    added console.log(result)

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,6 +19,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
+  console.log(result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
