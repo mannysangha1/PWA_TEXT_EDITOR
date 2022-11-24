793445eba595c9e9c64319e6df9fe2da79f960a0
Manny <mannysangha020@gmail.com>
2022-11-23 16:57:15 -0800

    added plugins, CSS loaders and babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
-    ],
+            new HtmlWebpackPlugin({
+                    template: './index.html',
+            }),
+            new WebpackPwaManifest({
+                    name: 'JATE',
+                    short_name: 'JATE',
+                    description: 'Just Another Text Editor',
+                    display: 'standalone',
+                    background_color: '',
+                    theme_color: '',
+                    start_url: '',
+                    publicPath: '',
+                    fingerprints: '',
+                    inject: '',
+                    icons: [
+                      {
+                        src: path.resolve('src/images/logo.png'),
+                        sizes: [96, 128, 192, 256, 384, 512],
+                        destination: path.join('assets','icons'),
+
+                    },
+                  ],
+            }),
+            new InjectManifest({
+                    swSrc: './src-sw.js',
+                    swDest: 'service-workers.js',
+            }),
+          ],
 
     module: {
       rules: [
+              {
+                      test: /\.m?js$/,
+                      exclude: /node_modules/,
+                      use: {
+                              loader: 'babel-loader',
+                              options: {
+                                      presents: ['@babel/present-env'],
+                                      plugins: [
+                                              '@babel/plugin-proposal-object-rest-spread',
+                                              '@babel/transform-runtime',
+                                      ],
+                              },
+                      },
+              },
         
       ],
     },
